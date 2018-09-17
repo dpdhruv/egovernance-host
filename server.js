2645ddd8ab118b600f6d6bc0172b6e5858ca33aa
dpdhruv <dhruv_7693@yahoo.co.in>
2018-09-17 14:16:22 +0530

    update

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/egovernance-host')));
+app.use(express.static(path.join(__dirname, 'dist/egovernance')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/egovernance-host/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/egovernance/index.html'));
 });
 
 const port = process.env.PORT || 3000;
